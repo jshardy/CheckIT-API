f8794f41cd6be1f80e27ac286b52097a98234e68
Jeffrey <jeffo369@hotmail.com>
2019-01-16 01:37:19 -0800

    Added ItemsDB to DataContext

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -7,6 +7,6 @@ namespace CheckIT.API.Data
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
         public DbSet<User> Users { get; set; }
-        
+        public DbSet<Item> Items { get; set; }
     }
 }
