acf93523ed295258a9c6affc5f4352d87f744e52
Jeffrey <jeffo369@hotmail.com>
2019-01-16 01:35:14 -0800

    Item DTO Started

---
+++ Dtos/ItemForAddDto.cs
@@ -0,0 +1,12 @@
+namespace CheckIT.API.Dtos
+{
+    public class ItemForAddDto
+    {
+        public int Id { get; set; }
+		public int UPC { get; set; }
+		public float Price { get; set; }
+        [Required]
+        public string Name { get; set; }
+		public string Description { get; set; }
+    }
+}
