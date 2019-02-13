36b6f943bb14c0de302a07b51acd3edf536abc98
Jeffrey <jeffo369@hotmail.com>
2019-02-12 19:06:16 -0800

    Created Customer Binding Object

---
+++ Models/BindingTargets/CustomerData.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using CheckIT.API.Models;
+
+namespace CheckIT.API.Models.BindingTargets
+{
+    public class CustomerData
+    {
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string CompanyName { get; set; }
+		public bool IsCompany { get; set; }
+		public string PhoneNumber { get; set; }
+		public string Email { get; set; }
+		public int CustAddressID { get; set; }
+		public Address CustAddress { get; set; }
+
+		public int CustInvoiceID { get; set; }
+		public Invoice CustInvoice { get; set; }
+
+        public Customer CustomerDataBindOBJ => new Customer
+        {
+            FirstName = CustomerDataBindOBJ.FirstName,
+            LastName = CustomerDataBindOBJ.LastName,
+            CompanyName = CustomerDataBindOBJ.CompanyName,
+            IsCompany = CustomerDataBindOBJ.IsCompany,
+            PhoneNumber = CustomerDataBindOBJ.PhoneNumber,
+            Email = CustomerDataBindOBJ.Email,
+            CustAddressID = CustomerDataBindOBJ.CustAddressID,
+            CustAddress = CustomerDataBindOBJ.CustAddress,
+            CustInvoiceID = CustomerDataBindOBJ.CustInvoiceID,
+            CustInvoice = CustomerDataBindOBJ.CustInvoice
+        };
+    }
+}
--- Models/Permissions.cs
+++ Models/Permissions.cs
@@ -5,19 +5,27 @@ namespace CheckIT.API.Models
         public int Id { get; set; }
 
         //User Permission Permissions
-        public bool UserPermissions { get; set; }
+        public bool SetUserPermissions { get; set; }
+        public bool ViewUserPermissions { get; set; }
 
         //Inventory Permissions
         public bool AddIventory { get; set; }
         public bool ArchiveIventory { get; set; }
+        public bool UpdateInventory { get; set; }
 
         //Invoice Permissions
         public bool AddInvoice { get; set; }
         public bool ArchiveInvoice { get; set; }
+        public bool ViewInvoices { get; set; }
 
         //Location Permissions
         public bool AddLocation { get; set; }
         public bool DeleteLocation { get; set; }
+
+        //Alert Permissions
+        public bool AddAlert { get; set; }
+        public bool DeleteAlert { get; set; }
+        public bool UpdateAlert { get; set; }
 
         //Customer Permissions
        
