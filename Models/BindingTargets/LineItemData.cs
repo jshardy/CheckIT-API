38dd22d08f795874595b53fd1359a865307b79a5
Quinn <quinnwm@pocketinet.com>
2019-02-22 22:47:30 -0800

    Added LineItemData.cs

---
+++ Models/BindingTargets/LineItemData.cs
@@ -0,0 +1,9 @@
+namespace checkit.api.Models.BindingTargets
+{
+    public class LineItemData
+    {
+        public int Quantity { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
