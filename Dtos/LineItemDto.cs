0c839c1d610b4be4b1f44f6979a62e2bc2c01d96
Jeffrey <jeffo369@hotmail.com>
2019-03-12 14:01:35 -0700

    Updated LineItemDto

--- Dtos/LineItemDto.cs
+++ Dtos/LineItemDto.cs
@@ -1,11 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace CheckIT.API.Dtos
 {
     public class LineItemData
     {
         public int Id { get; set; }
-        // TODO: Add money attribute
         public int Quantity { get; set; }
+        [Column(TypeName="Money")]
         public decimal Price { get; set; }
         public int ItemId { get; set; }
+        public int InvoiceId { get; set; }
     }
 }
