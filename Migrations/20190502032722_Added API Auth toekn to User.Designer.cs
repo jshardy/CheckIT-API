805ebed94527640cbb9b8bb121fb1966bf307a93
Quinn <quinnwm@pocketinet.com>
2019-05-01 21:16:07 -0700

    Migration to add auth token to DB

--- Migrations/20190502032722_Added API Auth toekn to User.Designer.cs
+++ Migrations/20190502032722_Added API Auth toekn to User.Designer.cs
@@ -4,14 +4,16 @@ using CheckIT.API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CheckIT.API.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20190502032722_Added API Auth toekn to User")]
+    partial class AddedAPIAuthtoekntoUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
