1c135bb923f99e797606699d87ae2fe734467f50
Quinn <quinnwm@pocketinet.com>
2019-05-29 06:33:39 -0700

    added a log model

---
+++ Models/Log.cs
@@ -0,0 +1,9 @@
+namespace CheckIT.API.Models
+{
+    public class Log
+    {
+        public int log_Id { get; set; }
+        public string log_details { get; set; }
+        public string log_function_name { get; set; }
+    }
+}
